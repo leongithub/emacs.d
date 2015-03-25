0fe421ed205bc3dbc04a5853a32fcb8304d88db3
Leon <yjg.2016@gmail.com>
2015-03-25 11:48:54 +0800

    Update init.el ,隐藏设置字体高度
    在macbook下目前默认看着比较舒服

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
 ;; (set-default-font "幼圆-14") ; 设置字体-大小
 ;; 本来windows可以如上设置，但由于要传到github上，Windows和linux用同一套设置，ubuntu默认的字体还好，所以这种不太理想。用下面的达到设置字体大小的目的
 ;; The value is in 1/10pt, so 200 will give you 20pt, etc.
-(set-face-attribute 'default nil :height 200)
+;; (set-face-attribute 'default nil :height 200)
 
 (add-to-list 'load-path (expand-file-name "els" user-emacs-directory))
 ;(add-to-list 'load-path "~/.emacs.d/els") ; 把els路径加入到load-path，以后el插件就可以放放到这个目录
