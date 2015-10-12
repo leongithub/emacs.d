c5ddd0e4b61f185330ea180e9bd799b596c4f750
leongithub <yjg.2016@gmail.com>
2015-10-12 12:25:42 +0800

    add theme-changer for change theme

--- els/init-theme.el
+++ els/init-theme.el
@@ -1,3 +1,11 @@
 (require 'solarized-theme)
-(load-theme 'solarized-dark t)
+;(load-theme 'solarized-dark t)
 ;(load-theme 'solarized-light t)
+
+;; 用 theme－changer 实现，白天用 light 主题，晚上用 dark 主题
+;; 设置时区，可用 https://db-ip.com/ 查询自己 ip 地址
+(setq calendar-location-name "Zhejiang") 
+(setq calendar-latitude 30.2741)
+(setq calendar-longitude 120.155)
+(require 'theme-changer)
+(change-theme 'solarized-light 'solarized-dark)
